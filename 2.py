88ec217fbe914ae0c302787c3c8e187d855f45df
jung <jung@appleui-MacBook-Pro.local>
2018-06-28 17:40:02 +0900

    first python file

---
+++ 2.py
@@ -0,0 +1,13 @@
+import math
+print(math.ceil(2.2))
+print(math.floor(2.7))
+print(math.pow(2,10))
+print(math.pi)
+
+print("Hello\t\tworld")
+print("Hello"*3)
+print("Hello"[0])
+print("Hello"*3)
+
+print("\a")
+print("\a")
